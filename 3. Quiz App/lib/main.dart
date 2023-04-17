94363e145a8f4125799a3b383ec441f379555ad2
Usama Mushtaq <usamabhattiengineer450@gmail.com>
2023-04-16 22:02:20 -0700

    Quiz App Uploaded

---
+++ 3. Quiz App/lib/main.dart
@@ -0,0 +1,311 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: QuizScreen());
+  }
+}
+class Question {
+  final String questionText;
+  final List<Answer> answersList;
+
+  Question(this.questionText, this.answersList);
+}
+
+class Answer {
+  final String answerText;
+  final bool isCorrect;
+
+  Answer(this.answerText, this.isCorrect);
+}
+
+List<Question> getQuestions() {
+  List<Question> list = [];
+  //ADD questions and answer here
+
+  list.add(Question(
+    "Who is the owner of Flutter?",
+    [
+      Answer("Nokia", false),
+      Answer("Samsung", false),
+      Answer("Google", true),
+      Answer("Apple", false),
+    ],
+  ));
+
+  list.add(Question(
+    "Who owns Iphone?",
+    [
+      Answer("Apple", true),
+      Answer("Microsoft", false),
+      Answer("Google", false),
+      Answer("Nokia", false),
+    ],
+  ));
+  list.add(Question(
+    "which language is use in flutter?",
+    [
+      Answer("C++", false),
+      Answer("C#", false),
+      Answer("Dart", true),
+      Answer("Python", false),
+    ],
+  ));
+
+  list.add(Question(
+    "What is the main function in a Flutter app?",
+    [
+      Answer("main()", true),
+      Answer("runApp()", false),
+      Answer(" build()", false),
+      Answer("create()", false),
+    ],
+  ));
+
+  list.add(Question(
+    "What is the command to create a new Flutter project?",
+    [
+      Answer(" flutter start project-name", false),
+      Answer(" flutter new project-name", false),
+      Answer("flutter init project-name", false),
+      Answer("flutter create project-name", true),
+    ],
+  ));
+  list.add(Question(
+    "Which IDEs are commonly used to develop apps with Flutter??",
+    [
+      Answer("Eclipse and NetBeans", false),
+      Answer("Sublime Text and Atom", false),
+      Answer("Android Studio and Visual Studio Code", true),
+      Answer(" Eclipse and NetBeans", false),
+    ],
+  ));
+
+  list.add(Question(
+    "What is the widget in Flutter?",
+    [
+      Answer("A graphical user interface component", true),
+      Answer("A database", false),
+      Answer("A programming language", false),
+      Answer("A networking protocol", false),
+    ],
+  ));
+
+
+  list.add(Question(
+    "Flutter user dart as a language?",
+    [
+      Answer("True", true),
+      Answer("False", false),
+    ],
+  ));
+  list.add(Question(
+    "my name is usama?",
+    [
+      Answer("True", true),
+      Answer("False", false),
+    ],
+  ));
+  list.add(Question(
+    "DR Abdullah is a flutter teacher?",
+    [
+      Answer("True", true),
+      Answer("False", false),
+    ],
+  ));
+  list.add(Question(
+    "Flutter is advance language?",
+    [
+      Answer("True", true),
+      Answer("False", false),
+    ],
+  ));
+  return list;
+}
+
+
+class QuizScreen extends StatefulWidget {
+  @override
+  State<QuizScreen> createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  //define the datas
+  List<Question> questionList = getQuestions();
+  int currentQuestionIndex = 0;
+  int score = 0;
+  Answer? selectedAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 5, 50, 80),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+        child:
+        Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          const Text(
+            "Simple Quiz App",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          _questionWidget(),
+          _answerList(),
+          _nextButton(),
+        ]),
+      ),
+    );
+  }
+
+  _questionWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Question ${currentQuestionIndex + 1}/${questionList.length.toString()}",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          padding: const EdgeInsets.all(32),
+          decoration: BoxDecoration(
+            color: Colors.orangeAccent,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Text(
+            questionList[currentQuestionIndex].questionText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  _answerList() {
+    return Column(
+      children: questionList[currentQuestionIndex]
+          .answersList
+          .map(
+            (e) => _answerButton(e),
+      )
+          .toList(),
+    );
+  }
+
+  Widget _answerButton(Answer answer) {
+    bool isSelected = answer == selectedAnswer;
+
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      height: 48,
+      child: ElevatedButton(
+        child: Text(answer.answerText),
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: isSelected ? Colors.orangeAccent : Colors.white,
+          onPrimary: isSelected ? Colors.white : Colors.black,
+        ),
+        onPressed: () {
+          if (selectedAnswer == null) {
+            if (answer.isCorrect) {
+              score++;
+            }
+            setState(() {
+              selectedAnswer = answer;
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  _nextButton() {
+    bool isLastQuestion = false;
+    if (currentQuestionIndex == questionList.length - 1) {
+      isLastQuestion = true;
+    }
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: 48,
+      child: ElevatedButton(
+        child: Text(isLastQuestion ? "Submit" : "Next"),
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: Colors.blueAccent,
+          onPrimary: Colors.white,
+        ),
+        onPressed: () {
+          if (isLastQuestion) {
+            //display score
+
+            showDialog(context: context, builder: (_) => _showScoreDialog());
+          } else {
+            //next question
+            setState(() {
+              selectedAnswer = null;
+              currentQuestionIndex++;
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  _showScoreDialog() {
+    bool isPassed = false;
+
+    if (score >= questionList.length * 0.6) {
+      //pass if 60 %
+      isPassed = true;
+    }
+    String title = isPassed ? "Passed " : "Failed";
+
+    return AlertDialog(
+      title: Text(
+        title + " | Score is $score",
+        style: TextStyle(color: isPassed ? Colors.green : Colors.redAccent),
+      ),
+      content: ElevatedButton(
+        child: const Text("Restart"),
+        onPressed: () {
+          Navigator.pop(context);
+          setState(() {
+            currentQuestionIndex = 0;
+            score = 0;
+            selectedAnswer = null;
+          });
+        },
+      ),
+    );
+  }
+}
